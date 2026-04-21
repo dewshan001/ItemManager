7e8c538f2368d5d49d8c30912169475aedc58400
Dewshan Gunawardhana <IT24103005@my.sliit.lk>
2026-04-21 13:21:01 +0530

    Project Files Added

---
+++ frontend/ItemManager/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { getItems } from "./api";
+import ItemForm from "./ItemForm";
+import ItemList from "./ItemList";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const res = await getItems();
+    setItems(res.data);
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto", fontFamily: "sans-serif" }}>
+      <h1>Item Manager</h1>
+      <ItemForm onItemAdded={fetchItems} />
+      <ItemList items={items} onItemDeleted={fetchItems} />
+    </div>
+  );
+}
+
+export default App;
